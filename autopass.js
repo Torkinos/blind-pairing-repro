40c92fdbbb8b405fc0c58a996c2071d8680fa307
Tornike Kapanadze <tornikekapan@gmail.com>
2025-09-22 16:17:08 +0300

    chore: add automatic timeout

--- autopass.js
+++ autopass.js
@@ -1,12 +1,18 @@
 import Autopass from 'autopass'
 import Corestore from 'corestore'
 
+setTimeout(() => {
+    console.log('timeout at 60s, exiting')
+    process.exit(0)
+}, 60000)
+
 // const pass = new Autopass(new Corestore('./pass'))
 
 // const inv = await pass.createInvite()
 // console.log('share to add', inv)
 
-const pair = Autopass.pair(new Corestore('./another-pass'), "mfv4i7yd3f5lzg361o/yryss5yma5unq16efy4u3bcsdzs4714f97sksgqttkr9b6wngfd1c8z4f6q7z7oyubuak6cmp6qc4p81u6y3jzsd86euitdsxwkdpqif6w")
+const pair = Autopass.pair(new Corestore('./another-pass'), "yryw1ztyxh4mnb3pns4egpi1itxa857p9twouugg3bhp6ztn5tqhkjtgw8uz9n9kthq4ctikhmpmi88q9cmnmmiuorj7yyus3a3bb1scoo")
+console.log('pairing...')
 
 const anotherPass = await pair.finished()
 console.log('another pass finished')
